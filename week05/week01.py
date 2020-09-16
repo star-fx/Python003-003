01219ddd6fc900565705fd31e849dedd8fee20f3
fanxing1 <fanxing1@lenovo.com>
2020-09-16 19:21:58 +0800

    to be continue

---
+++ week05/week01.py
@@ -0,0 +1,5 @@
+# 1. 读源码
+# open https://docs.python.org/3/library/functions.html#open
+
+# 2. 代码结构
+
---
+++ week05/week02.py
@@ -0,0 +1,6 @@
+# 1. 读源码
+# PEP-249 DB-API https://www.python.org/dev/peps/pep-0249/
+# Connection https://www.python.org/dev/peps/pep-0249/#connection-objects
+# Cursor https://www.python.org/dev/peps/pep-0249/#cursor-objects
+# MySQL Connector Python https://dev.mysql.com/doc/connector-python/en/
+
